47aec21cdd30fb9901fe0901e418253902c470ff
Shiying Lai <sl4849@columbia.edu>
2020-11-12 04:02:29 -0500

    Added working version of catbind function

--- qmss_sl_practice/qmss_sl_practice/qmss_sl_practice.py
+++ qmss_sl_practice/qmss_sl_practice/qmss_sl_practice.py
@@ -1,2 +1,8 @@
-import pandas as pd def catbind(a, b):concatenated = pd.concat([pd.Series(a.astype("str")),pd.Series(b.astype("str"))]) return pd.Categorical(concatenated)
+import pandas as pd
+def catbind(a, b):
+    concatenated = pd.concat([pd.Series(a.astype("str")),
+                   pd.Series(b.astype("str"))])
+    return pd.Categorical(concatenated)
+
+
 
