78671b0cfc99580bf305bc49068131a0f6feb720
shiyinglai <sl4849@columbia.edu>
2020-11-12 03:35:46 -0500

    update

--- qmss_sl_practice/qmss_sl_practice/qmss_sl_practice.py
+++ qmss_sl_practice/qmss_sl_practice/qmss_sl_practice.py
@@ -1,7 +1,3 @@
-import pandas
-
-def catbind(a, b):
-  concatenated = pd.concat([pd.Series(a.astype("str")),
-                            pd.Series(b.astype("str"))]) 
-  return pd.Categorical(concatenated)
+import pandas as pd def catbind(a, b):concatenated = pd.concat([pd.Series(a.astype("str")),
+pd.Series(b.astype("str"))]) return pd.Categorical(concatenated)
 
