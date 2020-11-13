72d1208660bbaaf0c27858cc83c07db9961c04fe
Shiying Lai <sl4849@columbia.edu>
2020-11-13 06:28:28 -0500

    update

--- qmss_sl_practice/tests/test_qmss_sl_practice.py
+++ qmss_sl_practice/tests/test_qmss_sl_practice.py
@@ -1,5 +1,10 @@
-from qmss_sl_practice import __version__
 from qmss_sl_practice import qmss_sl_practice
+import pandas as pd
 
-def test_version():
-    assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((qmss_sl_practice.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+
+    assert ((qmss_sl_practice.catbind(a, b)).categories == ["but", "character","counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
